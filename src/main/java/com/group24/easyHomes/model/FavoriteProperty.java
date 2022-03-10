ee460886d32abfccdb7fcd4f72cb5eb719e58f7a
Qinyue Wang <qn642785@dal.ca>
2022-03-09 22:53:06 -0400

    Add entities

--- src/main/java/com/group24/easyHomes/model/FavoriteProperty.java
+++ src/main/java/com/group24/easyHomes/model/FavoriteProperty.java
@@ -1,8 +1,16 @@
 package com.group24.easyHomes.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "FavoriteProperty")
 public class FavoriteProperty {
     private Long userId;
     private int propertyId;
+
+    @Id
+    @GeneratedValue
+    private Integer id;
 
     public FavoriteProperty(Long userId, int propertyId) {
         this.userId = userId;
@@ -26,5 +34,13 @@ public class FavoriteProperty {
 
     public void setPropertyId(int propertyId) {
         this.propertyId = propertyId;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
     }
 }
--- src/main/java/com/group24/easyHomes/model/FavoriteService.java
+++ src/main/java/com/group24/easyHomes/model/FavoriteService.java
@@ -1,8 +1,15 @@
 package com.group24.easyHomes.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "FavoriteService")
 public class FavoriteService {
     private Long userId;
     private int serviceId;
+    @Id
+    @GeneratedValue
+    private Integer id;
 
     public FavoriteService(Long userId, int serviceId) {
         this.userId = userId;
@@ -26,5 +33,13 @@ public class FavoriteService {
 
     public void setServiceId(int serviceId) {
         this.serviceId = serviceId;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
     }
 }
